d985fe1b331332f8005f3432c370f6586a3b4b36
Sandro Coelho <sandro.icoelhos@gmail.com>
2017-12-06 22:22:29 -0200

    .

--- localhost_SDPart2_jar_1.0-SNAPSHOT/src/main/java/Handlers/Server.java
+++ localhost_SDPart2_jar_1.0-SNAPSHOT/src/main/java/Handlers/Server.java
@@ -1,17 +1,61 @@
 package Handlers;
 
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
 import org.apache.thrift.TException;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TThreadPoolServer;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TServerTransport;
+import io.atomix.catalyst.transport.Address;
+import io.atomix.catalyst.transport.netty.NettyTransport;
+import io.atomix.copycat.server.CopycatServer;
+import io.atomix.copycat.server.StateMachine;
+import io.atomix.copycat.server.storage.Storage;
+import io.atomix.copycat.server.storage.StorageLevel;
 
-public class Server {
+public class Server extends StateMachine{
 
     public static Handler handler;
     public static Grafo.Thrift.Processor processor;
     
     public static void main(String[] args) {
+        
+        int myId = Integer.parseInt(args[0]);
+    	List<Address> addresses = new LinkedList<>();
+    	
+    	for(int i = 1; i <args.length; i+=2)
+    	{
+            Address address = new Address(args[i], Integer.parseInt(args[i+1]));
+    		addresses.add(address);
+    	}
+    		
+        CopycatServer.Builder builder = CopycatServer.builder(addresses.get(myId)).withStateMachine(Server::new)
+                                                     .withTransport( NettyTransport.builder()
+                                                                     .withThreads(4)
+                                                                     .build())
+                                                     .withStorage( Storage.builder()
+                                                                   .withDirectory(new File("logs_"+myId)) //Must be unique
+                                                                   .withStorageLevel(StorageLevel.DISK)
+                                                                   .build());
+        CopycatServer copycatServer = builder.build();
+
+        Thread t = new Thread();
+        
+        if(myId == 0)
+        {
+            copycatServer.bootstrap().join();
+        }
+        else
+        {
+            copycatServer.join(addresses).join();
+        }
+        
+        
+        
+        
+        
         try {
             handler = new Handler(args);
             processor = new Grafo.Thrift.Processor(handler);
